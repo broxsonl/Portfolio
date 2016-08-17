3b015cabed493c7b06704e353b5c4f2c9fbe197a
Lee Broxson <broxsonl@gmail.com>
2016-08-16 17:11:30 -0700

    Stylesheets, scripts, etc built out. Built 4 project objects in codeProjects.js in an array.

---
+++ scripts/codeProjects.js
@@ -0,0 +1,25 @@
+var projectsObjectsArray = [
+  {
+    name:               'about_me',
+    title:              'Code 201 About_Me Project',
+    projectUrl:         'https://broxsonl.github.io/about_me/'
+    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=380&h=270'
+  },
+  {
+    name:               'salmoncookies',
+    title:              'Code 201 Salmon Cookie Project',
+    projectUrl:         'https://broxsonl.github.io/salmoncookies/'
+    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=380&h=270'
+  },
+  {
+    name:               'busmall',
+    title:              'Code 201 Bus Mall Project',
+    projectUrl:         'https://broxsonl.github.io/busmall'
+    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=380&h=270'
+  },
+  {
+    name:               'firstdayofskeewl',
+    title:              'Code 201 FirstDayofSkeewl Project',
+    projectUrl:         'https://broxsonl.github.io/FirstDayofSkeewl/'
+    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=380&h=270'
+  },
