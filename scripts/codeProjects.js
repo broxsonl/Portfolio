0a89e53a2d1cc4d2c0c5e8f1b2b82f345a83f9f3
Lee Broxson <broxsonl@gmail.com>
2016-08-16 19:10:53 -0700

    Finished out the constructors, passed in object parameters, have new articles successfully constructing to page. Also completed stretch goal.

--- scripts/codeProjects.js
+++ scripts/codeProjects.js
@@ -1,25 +1,38 @@
-var projectsObjectsArray = [
+var projectObjectsArray = [
   {
     name:               'about_me',
     title:              'Code 201 About_Me Project',
-    projectUrl:         'https://broxsonl.github.io/about_me/'
-    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=380&h=270'
+    subtitle:           'subtitle stuff here',
+    projectUrl:         'https://broxsonl.github.io/about_me/',
+    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97280&w=400&h=280',
+    imageAlt:           'about_me project pic',
+    updatedOn:        '2015-07-22',
   },
   {
     name:               'salmoncookies',
     title:              'Code 201 Salmon Cookie Project',
-    projectUrl:         'https://broxsonl.github.io/salmoncookies/'
-    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=380&h=270'
+    subtitle:           'subtitle stuff here',
+    projectUrl:         'https://broxsonl.github.io/salmoncookies/',
+    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97280&w=400&h=280',
+    imageAlt:           'salmoncookie project pic',
+    updatedOn:        '2016-07-29',
   },
   {
     name:               'busmall',
     title:              'Code 201 Bus Mall Project',
-    projectUrl:         'https://broxsonl.github.io/busmall'
-    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=380&h=270'
+    subtitle:           'subtitle stuff here',
+    projectUrl:         'https://broxsonl.github.io/busmall',
+    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97280&w=400&h=280',
+    imageAlt:           'busmall project pic',
+    updatedOn:        '2016-08-05',
   },
   {
     name:               'firstdayofskeewl',
     title:              'Code 201 FirstDayofSkeewl Project',
-    projectUrl:         'https://broxsonl.github.io/FirstDayofSkeewl/'
-    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=28&txt=300%C3%97300&w=380&h=270'
-  },
+    subtitle:           'subtitle stuff here',
+    projectUrl:         'https://broxsonl.github.io/FirstDayofSkeewl/',
+    projectImage:       'https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97280&w=400&h=280',
+    imageAlt:           'firstdayofskeewl project pic',
+    updatedOn:        '2016-08-12',
+  }
+];
---
+++ scripts/project.js
@@ -0,0 +1,38 @@
+var projects = [];
+
+function Project (opts) {
+  // Save ALL the properties of `opts` into `this`
+  this.name = opts.name;
+  this.title = opts.title;
+  this.subtitle = opts.subtitle;
+  this.projectUrl = opts.projectUrl;
+  this.projectImage = opts.projectImage;
+  this.imageAlt = opts.imageAlt;
+  this.updatedOn = opts.updatedOn;
+};
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.find('h1').text(this.title);
+  $newProject.attr('data-name', this.name);
+  $newProject.find('p').text(this.subtitle);
+  $newProject.find('.project-picture a').attr('href', this.projectUrl);
+  $newProject.find('.project-picture a img').attr('src', this.projectImage);
+  $newProject.find('.project-picture a img').attr('alt', this.imageAlt);
+  $newProject.find('time[pubdate]').attr('title', this.updatedOn);
+  $newProject.find('time').html('last updated ' + parseInt((new Date() - new Date(this.updatedOn))/60/60/24/1000) + ' days ago');
+  $newProject.removeClass('template');
+  return $newProject;
+};
+
+projectObjectsArray.sort(function(firstElement, secondElement) {
+  return (new Date(secondElement.updatedOn)) - (new Date(firstElement.updatedOn));
+});
+
+projectObjectsArray.forEach(function(theCurrentArticleObject) {
+  projects.push(new Project(theCurrentArticleObject));
+});
+
+projects.forEach(function(project) {
+  $('#project-header').append(project.toHtml());
+});
