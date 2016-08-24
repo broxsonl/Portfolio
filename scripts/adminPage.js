803a6b2b6560ad818770bcf125a264cda609f2cb
Lee Broxson <broxsonl@gmail.com>
2016-08-23 22:52:13 -0700

    Used map to create an array of the quoteAuthors, and reduce to pull two specific object properties from it for use in our awesomestats

--- scripts/adminPage.js
+++ scripts/adminPage.js
@@ -1,10 +1,40 @@
 'use strict';
-projectView.renderAdminPage = function() {
-  var awesomeRender = Handlebars.compile($('#awesome-template').html());
-  Project.awesomePointsByTestimonial().forEach(function(awesomePointsObj) {
-    $('.awesome-points').append(awesomeRender(awesomePointsObj));
-  });
-  $('#testimonial-total').text(Testimonial.allTestimonials.length);
-};
+(function(module) {
+  Testimonial.renderAdminPage = function() {
+    var awesomeRender = Handlebars.compile($('#awesome-template').html());
+    Testimonial.awesomePointsByTestimonial().forEach(function(awesomePointsObj) {
+      $('.awesome-points').append(awesomeRender(awesomePointsObj));
+    });
+    $('#testimonial-total').text(Testimonial.allTestimonials.length);
+  };
 
-Project.fetchall(projectView.renderAdminPage);
+  Testimonial.fetchall(Testimonial.renderAdminPage);
+
+  Testimonial.allTestimonialAuthors = function() {
+    return Testimonial.allTestimonials.map(function(currentAuthor) {
+      return currentAuthor.quoteAuthor;
+    }).reduce(function(acc, cur) {
+      if (acc.indexOf(cur) === -1) {
+        acc.push(cur);
+      }
+      return acc;
+    }, []);
+  };
+
+  Testimonial.awesomePointsByAuthor = function() {
+    return Testimonial.allTestimonialAuthors().map(function(currentAuthor) {
+      return {
+        name: currentAuthor,
+        awesomePoints: Testimonial.allTestimonials.filter(function(currentTestimonial){
+          return currentTestimonial.quoteAuthor === currentAuthor;
+        }).map(function(currentTestimonial) {
+          return currentTestimonial.awesomePoints;
+        }).reduce(function(prev, cur) {
+          return prev + cur;
+        })
+      };
+    });
+  };
+
+  module.Testimonial = Testimonial;
+})(window);
--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,39 +1,43 @@
 'use strict';
+(function(module) {
 
-var projectView = {};
+  var projectView = {};
 
-projectView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    var $clickedOn = $(this).attr('data-content');
-    $('.tab-content').hide();
-    $('#' + $clickedOn).fadeIn('slow');
-  });
-  $('.main-nav .tab:first').click();
-};
+  projectView.handleMainNav = function() {
+    $('.main-nav').on('click', '.tab', function() {
+      var $clickedOn = $(this).attr('data-content');
+      $('.tab-content').hide();
+      $('#' + $clickedOn).fadeIn('slow');
+    });
+    $('.main-nav .tab:first').click();
+  };
 
-projectView.setTeasers = function() {
-  $('.project-picture').hide();
-
-  $('#projects').on('click', '.see-more', function(e) {
-    e.preventDefault();
-    $(this).parent().find('*').fadeIn();
-    $(this).html('See less &larrhk;').removeClass('see-more').addClass('see-less');
-  });
-
-  $('#projects').on('click', '.see-less', function(event){
-    event.preventDefault();
+  projectView.setTeasers = function() {
     $('.project-picture').hide();
-    $(this).html('Read on &#x21aa').removeClass('see-less').addClass('see-more');
 
-  });
-};
+    $('#projects').on('click', '.see-more', function(e) {
+      e.preventDefault();
+      $(this).parent().find('*').fadeIn();
+      $(this).html('See less &larrhk;').removeClass('see-more').addClass('see-less');
+    });
 
-projectView.renderIndexPage = function() {
-  Project.allProjects.forEach(function(a) {
-    $('#projects').append(a.toHtml());
-  });
-  projectView.handleMainNav();
-  projectView.setTeasers();
-};
+    $('#projects').on('click', '.see-less', function(event){
+      event.preventDefault();
+      $('.project-picture').hide();
+      $(this).html('Read on &#x21aa').removeClass('see-less').addClass('see-more');
 
-Project.fetchAll();
+    });
+  };
+
+  projectView.renderIndexPage = function() {
+    Project.allProjects.forEach(function(a) {
+      $('#projects').append(a.toHtml());
+    });
+    projectView.handleMainNav();
+    projectView.setTeasers();
+  };
+
+  Project.fetchAll();
+
+  module.projectView = projectView;
+})(window);
