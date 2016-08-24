594e578ade68d5cc5c0adbf60f3ab25860c430d1
Lee Broxson <broxsonl@gmail.com>
2016-08-23 23:48:26 -0700

    Worked on getting the Admin page to render to no avail. WIll need to work with teacher and TAs to fix.

--- scripts/adminPage.js
+++ scripts/adminPage.js
@@ -1,40 +1,37 @@
 'use strict';
-(function(module) {
-  Testimonial.renderAdminPage = function() {
-    var awesomeRender = Handlebars.compile($('#awesome-template').html());
-    Testimonial.awesomePointsByTestimonial().forEach(function(awesomePointsObj) {
-      $('.awesome-points').append(awesomeRender(awesomePointsObj));
-    });
-    $('#testimonial-total').text(Testimonial.allTestimonials.length);
-  };
+Testimonial.renderAdminPage = function() {
+  var source = $('#testimonial-template').html();
+  var awesomeRender = Handlebars.compile(source);
+  Testimonial.awesomePointsByTestimonial().forEach(function(awesomePointsObj) {
+    $('document .awesome-points').append(awesomeRender(awesomePointsObj));
+  });
+  $('#testimonial-total').text(Testimonial.allTestimonials.length);
+};
 
-  Testimonial.fetchall(Testimonial.renderAdminPage);
+Testimonial.allTestimonialAuthors = function() {
+  return Testimonial.allTestimonials.map(function(currentAuthor) {
+    return currentAuthor.quoteAuthor;
+  }).reduce(function(acc, cur) {
+    if (acc.indexOf(cur) === -1) {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+};
 
-  Testimonial.allTestimonialAuthors = function() {
-    return Testimonial.allTestimonials.map(function(currentAuthor) {
-      return currentAuthor.quoteAuthor;
-    }).reduce(function(acc, cur) {
-      if (acc.indexOf(cur) === -1) {
-        acc.push(cur);
-      }
-      return acc;
-    }, []);
-  };
+Testimonial.awesomePointsByAuthor = function() {
+  return Testimonial.allTestimonialAuthors().map(function(currentAuthor) {
+    return {
+      name: currentAuthor,
+      awesomePoints: Testimonial.allTestimonials.filter(function(currentTestimonial){
+        return currentTestimonial.quoteAuthor === currentAuthor;
+      }).map(function(currentTestimonial) {
+        return currentTestimonial.awesomePoints;
+      }).reduce(function(prev, cur) {
+        return prev + cur;
+      })
+    };
+  });
+};
 
-  Testimonial.awesomePointsByAuthor = function() {
-    return Testimonial.allTestimonialAuthors().map(function(currentAuthor) {
-      return {
-        name: currentAuthor,
-        awesomePoints: Testimonial.allTestimonials.filter(function(currentTestimonial){
-          return currentTestimonial.quoteAuthor === currentAuthor;
-        }).map(function(currentTestimonial) {
-          return currentTestimonial.awesomePoints;
-        }).reduce(function(prev, cur) {
-          return prev + cur;
-        })
-      };
-    });
-  };
-
-  module.Testimonial = Testimonial;
-})(window);
+// Testimonial.fetchall();
